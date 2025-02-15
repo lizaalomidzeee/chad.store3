36ec88593c498921b8280b351adf8f5e2da54fe0
lizaalomidzeee <lomidzelizi997@gmail.com>
2025-02-15 15:37:25 +0400

    serialiers

--- products/models.py
+++ products/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-from config.model_utils.models import TimeStampedModel 
+from config.util_models.models import TimeStampModel
 from products.choices import Currency
 
-class Product(TimeStampedModel, models.Model):
+class Product(TimeStampModel, models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
@@ -15,27 +15,28 @@ class Product(TimeStampedModel, models.Model):
         pass
 
 
-class Review(TimeStampedModel, models.Model):
+class Review(TimeStampModel, models.Model):
     product = models.ForeignKey('products.Product', related_name='reviews', on_delete=models.CASCADE)
     user = models.ForeignKey('users.User', related_name='reviews', on_delete=models.SET_NULL, null=True, blank=True)
     content = models.TextField()
     rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
 
 
-class FavoriteProduct(TimeStampedModel, models.Model):
+class FavoriteProduct(TimeStampModel, models.Model):
     product = models.ForeignKey('products.Product', related_name='favorite_products', on_delete=models.CASCADE)
     user = models.ForeignKey('users.User', related_name='favorite_products', on_delete=models.SET_NULL, null=True, blank=True)
 
 
-class ProductTag(TimeStampedModel, models.Model):
+class ProductTag(TimeStampModel, models.Model):
     name = models.CharField(max_length=255, unique=True)
 
 
-class Cart(TimeStampedModel, models.Model):
+class Cart(TimeStampModel, models.Model):
     products = models.ManyToManyField('products.Product', related_name='carts')
     user = models.OneToOneField('users.User', related_name='cart', on_delete=models.SET_NULL, null=True, blank=True)
 
 
-class ProductImage(TimeStampedModel, models.Model):
+class ProductImage(TimeStampModel, models.Model):
     image = models.ImageField(upload_to='products/')
     product = models.ForeignKey('products.Product', related_name='images', on_delete=models.CASCADE)
+    
--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from products.models import Review, Product
+from products.models import Review, Product, ProductTag, FavoriteProduct, Cart
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -30,3 +30,63 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         exclude = ['created_at', 'updated_at', 'tags'] 
         model = Product
+        
+        
+        
+class ProductTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductTag
+        fields = ['id', 'name']
+        
+        
+        
+class FavoriteProductSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    product_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = FavoriteProduct
+        fields = ['id', 'user', 'product_id', 'product']
+        read_only_fields = ['id', 'product']
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Invalid product_id. Product does not exist.")
+        return value
+
+    def create(self, validated_data):
+        product_id = validated_data.pop('product_id')
+        user = validated_data.pop('user')
+        
+        product = Product.objects.get(id=product_id)
+        favorite, created = FavoriteProduct.objects.get_or_create(user=user, product=product)
+
+        if not created:
+            raise serializers.ValidationError("This product is already in favorites.")
+
+        return favorite
+    
+
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    products = ProductSerializer(many=True, read_only=True)
+    product_ids = serializers.PrimaryKeyRelatedField(
+        source='products',
+        queryset=Product.objects.all(),
+        many=True, 
+        write_only=True
+    )
+
+    class Meta:
+        model = Cart
+        fields = ['user', 'product_ids', 'products']
+
+    def create(self, validated_data):
+        user = validated_data.get('user')
+        products = validated_data.pop('products')
+
+        cart, _ = Cart.objects.get_or_create(user=user)
+        cart.products.add(*products) 
+
+        return cart
